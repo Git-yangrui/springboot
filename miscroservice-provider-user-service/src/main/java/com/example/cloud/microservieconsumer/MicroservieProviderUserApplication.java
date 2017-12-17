f7200655f2b37b0c5fc26ba9a4866f5a0ca027f4
yangrui <645374132@qq.com>
2017-12-17 14:59:47 +0800

    refactor

--- miscroservice-provider-user-service/src/main/java/com/example/cloud/microservieconsumer/MicroservieProviderUserApplication.java
+++ miscroservice-provider-user-service/src/main/java/com/example/cloud/microservieconsumer/MicroservieProviderUserApplication.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class MicroservieProviderApplication {
+public class MicroservieProviderUserApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MicroservieProviderApplication.class, args);
+		SpringApplication.run(MicroservieProviderUserApplication.class, args);
 	}
 }
