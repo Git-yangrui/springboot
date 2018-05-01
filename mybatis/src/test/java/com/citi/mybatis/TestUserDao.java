3a322d558b96bd83223300ae79fc3c30d73a4223
yangrui <645374132@qq.com>
2018-05-01 23:23:46 +0800

    refactor

--- mybatis/src/test/java/com/citi/mybatis/TestUserDao.java
+++ mybatis/src/test/java/com/citi/mybatis/TestUserDao.java
@@ -1,5 +1,6 @@
 package com.citi.mybatis;
 
+import com.citi.entities.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mybatis.spring.annotation.MapperScan;
@@ -11,9 +12,7 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = {DataSourceAutoConfiguration.class, MybatisAutoConfiguration.class})
@@ -29,20 +28,22 @@ public class TestUserDao {
 
     @Test
     public void test1() throws Exception {
-        Connection connection = dataSource.getConnection();
-        Statement statement = connection.createStatement();
-        ResultSet resultSet1 = statement.executeQuery("select * from USER");
+        List<User> allUser = userMapper.getAllUser();
 
-        while (resultSet1.next()) {
-            int anInt = resultSet1.getInt(1);
-//          String string = resultSet1.getString("2");
-            int anInt1 = resultSet1.getInt(3);
+//        Connection connection = dataSource.getConnection();
+//        Statement statement = connection.createStatement();
+//        ResultSet resultSet1 = statement.executeQuery("select * from USER");
+//
+//        while (resultSet1.next()) {
+//            int anInt = resultSet1.getInt(1);
+////          String string = resultSet1.getString("2");
+//            int anInt1 = resultSet1.getInt(3);
+//
+//            System.out.println(anInt);
+//
+////          System.out.println(string);
+//            System.out.println(anInt1);
 
-            System.out.println(anInt);
 
-//          System.out.println(string);
-            System.out.println(anInt1);
-
-        }
     }
 }
